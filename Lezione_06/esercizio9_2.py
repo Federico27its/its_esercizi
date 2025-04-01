aa1cd2ee0cb9a8011cdcd4fc86aa877507a61889
Federico27its <Federico27.its@gmail.com>
2025-04-01 11:16:11 +0200

    progettazione_sistemi

---
+++ Lezione_06/esercizio9_2.py
@@ -0,0 +1,10 @@
+'''9-2. Three Restaurants: Start with your class from Exercise 9-1.
+Create three different instances from the class, and call describe_restaurant() for each instance.'''
+from esercizio9_1 import Restaurant
+
+r1: Restaurant = Restaurant("Da Gigione", "giapponese")
+r2: Restaurant = Restaurant("Da Paolone", "cinese")
+r3: Restaurant = Restaurant("Da Sergione", "vietnamita")
+r1.describe_redtaurant()
+r2.describe_redtaurant()
+r3.describe_redtaurant()
